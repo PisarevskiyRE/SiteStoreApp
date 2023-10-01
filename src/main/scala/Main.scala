d75fa00ebd70f5dc95a4ef3291c2d1146bc7d5aa
Pisarevskiy.RE <daimon--@live.ru>
2023-10-02 00:06:54 +0300

    finish

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -53,7 +53,6 @@ object Main extends App {
     .filter(new FilterEvents[Event](Constants.events))
 
 
-
   val processStream: SingleOutputStreamOperator[Item] = clearStraem
     .keyBy(new EventKeySelector[Event])
     .window(
@@ -74,17 +73,15 @@ object Main extends App {
   val outTopShow = processStream.getSideOutput(Constants.topShow)
 
 
-  val host = new HttpHost("127.0.0.1", 9200, "http")
+  val host = Constants.elasticHost
   val sinkBay = new SinkElastic(host).getSink("idx_bay")
   val sinkShow = new SinkElastic(host).getSink("idx_show")
   val sinkFull = new SinkElastic(host).getSinkFull("idx_full")
+
 
   outTopBay.sinkTo(sinkBay)
   outTopShow.sinkTo(sinkShow)
   processStream.sinkTo(sinkFull)
 
-
-
   env.execute()
-
 }
--- src/main/scala/scheme/Constants.scala
+++ src/main/scala/scheme/Constants.scala
@@ -1,6 +1,7 @@
 package scheme
 
 import org.apache.flink.util.OutputTag
+import org.apache.http.HttpHost
 
 object Constants {
   val defaultFormat = "yyyy-MM-dd HH:mm:ss"
@@ -8,7 +9,7 @@ object Constants {
   val kafkaHost = "localhost:9092"
   val kafkaTopic = "user_behavior"
   val kafkaLabel = "Kafka-Source"
-  val events: Seq[String] = Seq("buy","pv")
+  val events: Seq[String] = Seq(eventBay,eventShow)
   val eventBay = "buy"
   val eventShow = "pv"
   val lenWindow: Int = 60
@@ -17,5 +18,7 @@ object Constants {
 
   val topBay = new OutputTag[OutputItem]("top-bay-side-output") {}
   val topShow = new OutputTag[OutputItem]("top-show-side-output") {}
+
+  val elasticHost = new HttpHost("127.0.0.1", 9200, "http")
 
 }
